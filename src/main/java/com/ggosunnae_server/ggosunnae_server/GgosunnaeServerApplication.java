5cd996cd154dc14264fa442e72a2437a682817c0
ehdwoKIM <kinbell19@gmail.com>
2024-07-18 19:26:30 +0900

    boiler plate

---
+++ src/main/java/com/ggosunnae_server/ggosunnae_server/GgosunnaeServerApplication.java
@@ -0,0 +1,13 @@
+package com.ggosunnae_server.ggosunnae_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgosunnaeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgosunnaeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggosunnae_server/ggosunnae_server/GgosunnaeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggosunnae_server.ggosunnae_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgosunnaeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
